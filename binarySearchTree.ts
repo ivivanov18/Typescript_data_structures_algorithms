3e75e09d4b85ed369db5b1b5cb4416f1edfd250d
ivivanov18 <ivanov.ivan.v@gmail.com>
2020-06-05 23:47:30 +0300

    - fix issue with TreeNode or null not able to be able assigned to Treenod

--- binarySearchTree.ts
+++ binarySearchTree.ts
@@ -17,139 +17,142 @@
  */
 
 class TreeNode {
-    value: number;
-    left: TreeNode | null;
-    right: TreeNode | null;
-    constructor(value: number) {
-        this.value = value;
-        this.left = null;
-        this.right = null;
-    }
+  value: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(value: number) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
 }
 
 export class BinarySearchTree {
-    root: TreeNode | null;
-    constructor() {
-        this.root = null;
+  root: TreeNode | null;
+  constructor() {
+    this.root = null;
+  }
+
+  /**
+   * Function that inserts a new node in the tree with the value passed
+   * @param {number} value
+   * @return {BinarySearchTree} - returns `this` object to be able to chain insert calls
+   */
+  insert(value: number): BinarySearchTree {
+    if (this.root == null) {
+      this.root = new TreeNode(value);
+      return this;
     }
+    let current = this.root;
+    while (true) {
+      if (value == current.value) return this;
+      else if (value < current.value) {
+        if (current.left == null) {
+          current.left = new TreeNode(value);
+          return this;
+        } else {
+          current = current.left;
+        }
+      } else {
+        if (current.right == null) {
+          current.right = new TreeNode(value);
+          return this;
+        } else {
+          current = current.right;
+        }
+      }
+    }
+  }
+
+  /**
+   * Function that checks whether the BST contains a given value
+   * @param {string} value
+   * @return {boolean}
+   */
+  contains(value: number): boolean {
+    if (this.root == null) return false;
+    let current = this.root;
+    while (current) {
+      if (value < current.value && current.left) {
+        current = current.left;
+      } else if (value > current.value && current.right) {
+        current = current.right;
+      } else return true;
+    }
+    return false;
+  }
+
+  /**
+   * Function that finds the node in the BST that contains the passed value, null if not inside
+   * @param {string} value
+   * @return {TreeNode | null}
+   */
+  find(value: number): TreeNode | null {
+    if (this.root == null) return null;
+    let current = this.root;
+    let found = false;
+    while (current && !found) {
+      if (value < current.value) current = current.left!;
+      else if (value > current.value) current = current.right!;
+      else found = true;
+    }
+    if (!found) return null;
+    return current;
+  }
+
+  breadFirstSearch(): Array<number> {
+    const visited = Array<number>();
+    const toBeVisited = Array<TreeNode>(); // queue type structure
+    let visitingNode = this.root;
+    if (visitingNode != null) {
+      toBeVisited.push(visitingNode!);
+    }
+    while (toBeVisited.length) {
+      visitingNode = toBeVisited.shift()!;
+      visited.push(visitingNode?.value);
+      if (visitingNode?.left) toBeVisited.push(visitingNode?.left);
+      if (visitingNode?.right) toBeVisited.push(visitingNode?.right);
+    }
+    return visited || [];
+  }
+
+  depthSearchFirstPreOrder(): Array<number> {
+    const visited = Array<number>();
+    let current = this.root;
+    if (current != null) {
+      traverse(current!);
+    }
+
+    return visited;
 
     /**
-     * Function that inserts a new node in the tree with the value passed
-     * @param {number} value
-     * @return {BinarySearchTree} - returns `this` object to be able to chain insert calls
+     * Helper function that checks whether left/right node exists and traverse them also
+     * @param treeNode
      */
-    insert(value: number): BinarySearchTree {
-        if (this.root == null) {
-            this.root = new TreeNode(value);
-        }
-        let current = this.root;
-        while (true) {
-            if (value == current.value) return this;
-            else if (value < current.value) {
-                if (current.left == null) {
-                    current.left = new TreeNode(value);
-                    return this;
-                } else {
-                    current = current.left;
-                }
-            } else {
-                if (current.right == null) {
-                    current.right = new TreeNode(value);
-                    return this;
-                } else {
-                    current = current.right;
-                }
-            }
-        }
+    function traverse(treeNode: TreeNode) {
+      visited.push(treeNode!.value);
+      if (treeNode!.left) traverse(treeNode!.left);
+      if (treeNode!.right) traverse(treeNode!.right);
     }
+  }
+
+  depthSearchFirstPostOrder(): Array<number> {
+    const visited = Array<number>();
+    let current = this.root;
+    if (current != null) {
+      traverse(current!);
+    }
+
+    return visited;
 
     /**
-     * Function that checks whether the BST contains a given value
-     * @param {string} value
-     * @return {boolean}
+     * Helper function that checks whether left/right node exists and traverse them also
+     * @param treeNode
      */
-    contains(value: number): boolean {
-        if (this.root == null) return false;
-        let current = this.root;
-        while (current) {
-            if (value < current.value) current = current.left;
-            else if (value > current.value) current = current.right;
-            else return true;
-        }
-        return false;
+    function traverse(treeNode: TreeNode) {
+      if (treeNode!.left) traverse(treeNode!.left);
+      if (treeNode!.right) traverse(treeNode!.right);
+      visited.push(treeNode!.value);
     }
-
-    /**
-     * Function that finds the node in the BST that contains the passed value, null if not inside
-     * @param {string} value
-     * @return {TreeNode | null}
-     */
-    find(value: number): TreeNode | null {
-        if (this.root == null) return null;
-        let current = this.root;
-        let found = false;
-        while (current && !found) {
-            if (value < current.value) current = current.left!;
-            else if (value > current.value) current = current.right!;
-            else found = true;
-        }
-        if (!found) return null;
-        return current;
-    }
-
-    breadFirstSearch(): Array<number> {
-        const visited = Array<number>();
-        const toBeVisited = Array<TreeNode>(); // queue type structure
-        let visitingNode = this.root;
-        if (visitingNode != null) {
-            toBeVisited.push(visitingNode!);
-        }
-        while (toBeVisited.length) {
-            visitingNode = toBeVisited.shift()!;
-            visited.push(visitingNode ?.value);
-            if (visitingNode ?.left) toBeVisited.push(visitingNode ?.left);
-            if (visitingNode ?.right) toBeVisited.push(visitingNode ?.right);
-        }
-        return visited || [];
-    }
-
-    depthSearchFirstPreOrder(): Array<number> {
-        const visited = Array<number>();
-        let current = this.root;
-        if (current != null) {
-            traverse(current!);
-        }
-
-        return visited;
-
-        /**
-         * Helper function that checks whether left/right node exists and traverse them also
-         * @param treeNode 
-         */
-        function traverse(treeNode: TreeNode) {
-            visited.push(treeNode!.value);
-            if (treeNode!.left) traverse(treeNode!.left);
-            if (treeNode!.right) traverse(treeNode!.right);
-        }
-    }
-
-    depthSearchFirstPostOrder(): Array<number> {
-        const visited = Array<number>();
-        let current = this.root;
-        if (current != null) {
-            traverse(current!);
-        }
-
-        return visited;
-
-        /**
-         * Helper function that checks whether left/right node exists and traverse them also
-         * @param treeNode 
-         */
-        function traverse(treeNode: TreeNode) {
-            if (treeNode!.left) traverse(treeNode!.left);
-            if (treeNode!.right) traverse(treeNode!.right);
-            visited.push(treeNode!.value);
-        }
-    }
+  }
 }
